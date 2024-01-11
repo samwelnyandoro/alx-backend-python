535399376571e3c7b4f53e038b3ebfc558bc44c3
Samwel <samwelnyandoro01@gmail.com>
2024-01-11 06:35:03 -0800

    add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+type-annotated function
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''function that add two float and return their output
+    '''
+    return (a + b)
