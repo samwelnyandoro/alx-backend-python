f73e6cf6d5aa0303a39e175effc6dc76a76e0434
Samwel <samwelnyandoro01@gmail.com>
2024-01-11 06:36:54 -0800

    floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+type-annotated function
+'''
+import math
+
+
+def floor(n: float) -> int:
+    '''function that floor a float and return an integer
+    '''
+    return (math.floor(n))
