dfb9f96eaec67d6efbadfab7549dc64d3927c12c
Samwel <samwelnyandoro01@gmail.com>
2024-01-16 05:37:56 -0800

    3tasks.py

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Create a task"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return a task"""
+    return asyncio.create_task(wait_random(max_delay))
